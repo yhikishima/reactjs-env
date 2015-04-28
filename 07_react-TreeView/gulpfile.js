3ed397f6930d07fd28fa077583cafd9c0f134784
yasuaki-hikishima <yasuaki_hikishima@waku-2.com>
2015-04-29 00:44:57 +0900

    add gulpfile and index.html

--- 07_react-TreeView/gulpfile.js
+++ 07_react-TreeView/gulpfile.js
@@ -4,17 +4,45 @@ var $    = require('gulp-load-plugins')();
 
 gulp.task.('clean', function(cb) {
   var del = require('del');
+  del(['./dist']);
   cb();
 });
 
 gulp.task('build', function() {
+  var browserify = require('browserify');
+  var source     = require( 'vinyl-source-stream' );
+  var buffer     = require( 'vinyl-buffer' );
+
+  return browserify(
+      './src/js/main.js',
+      {
+        debug: true,
+        transform: [ 'reactify', 'debowerify' ]
+      }
+    )
+    .bundle()
+    .pipe( source( 'app.js' ) )
+    .pipe( buffer() )
+    .pipe( $.sourcemaps.init( { loadMaps: true } ) )
+    .pipe( $.uglify() )
+    .pipe( $.sourcemaps.write( './' ) )
+    .pipe( gulp.dest( './src/js' ) );
 
 });
 
-gulp.task('serve', function() {
-
+gulp.task('watch', ['build'], function() {
+  gulp.watch([
+    './src/js/*.js',
+    '!./src/js/app.js'
+  ], [ 'build' ]);
 });
 
-gulp.task('default', function() {
-
+gulp.task('serve', ['watch'], function() {
+  var connect     = require( 'connect' );
+  var serveStatic = require( 'serve-static' );
+  var app = connect();
+  app.use( serveStatic( __dirname ) );
+  app.listen( 8080 );
 });
+
+gulp.task('default', ['build']);
