a000979d646b5a32d3290c38bac2ff5c7a0092e9
yasuaki-hikishima <yasuaki_hikishima@waku-2.com>
2015-04-28 09:06:53 +0900

    create react treeView once

---
+++ 07_react-TreeView/gulpfile.js
@@ -0,0 +1,20 @@
+// gulpfile.js
+var gulp = require('gulp');
+var $    = require('gulp-load-plugins')();
+
+gulp.task.('clean', function(cb) {
+  var del = require('del');
+  cb();
+});
+
+gulp.task('build', function() {
+
+});
+
+gulp.task('serve', function() {
+
+});
+
+gulp.task('default', function() {
+
+});
