70f53fe25408c11ceb4496b1f23f222fa4d58380
yasuaki-hikishima <yasuaki_hikishima@waku-2.com>
2015-05-05 16:41:08 +0900

    create todo appJs component

--- 08_react-todo/src/js/app.js
+++ 08_react-todo/src/js/app.js
@@ -1,5 +1,50 @@
 // app.js
+var todos = [{
+  id: '_1',
+  name: 'Buy some milk',
+  done: true
+},{
+  id: '_2',
+  name: 'Birthday present to Alice',
+  done: false
+}];
+
+var Todo = React.createClass({
+  render: function() {
+    var todo = this.props.todo;
+    return (<li>{todo.name}<button>Done</button></li>);
+  }
+});
+
+var TodoList = React.createClass({
+  render: function() {
+    var rows = this.props.todos.filter(function(todo) {
+      return !todo.done;
+    }).map(function(todo) {
+      return (<Todo key={todo.id} todo={todo}></Todo>);
+    });
+    return (
+      <div className="active-todos">
+        <h2>Active</h2>
+        <ul>{rows}</ul>
+      </div>
+    );
+  }
+});
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h1>My Todo</h1>
+        <TodoList todos={todos}/>
+      </div>
+    );
+  }
+});
+
+
 React.render(
-  <h1>My TODO</h1>,
+  <App></App>,
   document.getElementById('app-container')
 );
